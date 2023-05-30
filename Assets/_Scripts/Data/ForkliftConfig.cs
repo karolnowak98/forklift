c2ee4e55e39c50b1f854fdc9635ce805c1d95f2f
Karol.N <karol.nowak@ideaplus.app>
2023-05-30 10:41:50 +0200

    Add base input & some new configurable variables

--- Assets/_Scripts/Data/ForkliftConfig.cs
+++ Assets/_Scripts/Data/ForkliftConfig.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
-
 namespace Data
 {
     [CreateAssetMenu(fileName = "Forklift Config", menuName = "Configs/Forklift Config")]
     public class ForkliftConfig : ScriptableObject
     {
-        [SerializeField] private float _movementSpeed;
-        [SerializeField] private float _liftingSpeed;
-        [SerializeField] private float _maxAngle;
-        [SerializeField] private float _maxKg;
+        [SerializeField, Range(0, 100)]
+        private float _movementSpeed;
+        
+        [SerializeField, Range(0, 100)]
+        private float _liftingSpeed;
+        
+        [SerializeField, Range(0, 80)]
+        private float _maxAngle;
+        
+        [SerializeField, Range(0, 100)]
+        private float _maxKg;
 
+        [SerializeField] 
+        private Vector2Int _liftMinMaxPositions;
+
+        public Vector2Int LiftMinMaxPositions => _liftMinMaxPositions;
         public float MovementSpeed => _movementSpeed;
         public float LiftingSpeed => _liftingSpeed;
         public float MaxAngle => _maxAngle;
--- Assets/_Scripts/Logic/Forklift.cs
+++ Assets/_Scripts/Logic/Forklift.cs
@@ -1,15 +1,27 @@
-﻿using Data;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using Data;
 
 namespace Logic
 {
     public class Forklift : MonoBehaviour
     {
-        [SerializeField] private ForkliftConfig _config;
+        [SerializeField] 
+        private ForkliftConfig _config;
 
-        public void Drive()
+        public void Drive(InputAction.CallbackContext context)
         {
             Debug.Log("Drive");
+        }
+        
+        public void LiftUp(InputAction.CallbackContext context)
+        {
+            Debug.Log("Lifting up...");
+        }
+        
+        public void Lower(InputAction.CallbackContext context)
+        {
+            Debug.Log("Lowering...");
         }
     }
 }
