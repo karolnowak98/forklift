f77481a04f69485fda516de14f7b7211bbfd5633
Karol.N <karol.nowak@ideaplus.app>
2023-06-04 21:12:16 +0200

    Configure whole config, optimize code

--- Assets/_Scripts/Forklift.cs
+++ Assets/_Scripts/Forklift.cs
@@ -1,26 +1,33 @@
-﻿using Data;
+﻿using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Mathematics;
 
-namespace Logic
+namespace Forklift
 {
-    public class VehicleController : MonoBehaviour
+    public class Forklift : MonoBehaviour
     {
+        [SerializeField] private ForkliftConfig _config;
+        [SerializeField] private Rigidbody _rb;
         [SerializeField] private Transform[] _frontWheels;
         [SerializeField] private Transform[] _backWheels;
-        [SerializeField] private Rigidbody _rb;
-        [SerializeField] private ForkliftConfig _config;
         [SerializeField] private Transform _forks;
         [SerializeField] private Transform _steeringWheel;
         
-        private Vector2 _movementInput;
         private Controls _controls;
+        private Vector2 _movementInput;
+        private float _rotationTimer;
+        private float _currentSpeed;
+        private float _targetSpeed;
+        private float _fixedDeltaTime;
         private bool _isRaisingForks;
         private bool _isLoweringForks;
 
         private void Awake()
         {
-            _rb.centerOfMass += Vector3.down * _config.CenterMassOffset;
+            _rb.centerOfMass += _config.CenterMassOffset;
+            _currentSpeed = 0f;
+            _targetSpeed = 0f;
             
             _controls = new Controls();
             _controls.Player.Drive.performed += OnMove;
@@ -29,6 +36,11 @@ namespace Logic
             _controls.Player.LiftRaise.canceled += OnForkliftRaise;
             _controls.Player.LiftLower.performed += OnForkliftLower;
             _controls.Player.LiftLower.canceled += OnForkliftLower;
+        }
+
+        private void Start()
+        {
+            _fixedDeltaTime = Time.fixedDeltaTime;
         }
 
         private void OnEnable()
@@ -46,7 +58,6 @@ namespace Logic
             ApplyMoveForce();
             ApplySteeringTorque();
             ApplyFriction();
-            ApplyWheelRotating();
             
             if (_isRaisingForks)
             {
@@ -59,17 +70,36 @@ namespace Logic
             }
         }
 
-        
+        private void Update()
+        {
+            ApplyWheelRotating();
+        }
+
         private void ApplyMoveForce()
         {
-            var movementForce = transform.forward * (_movementInput.y * _config.AcceleratingSpeed);
+            _targetSpeed = _movementInput.y * _config.MaxSpeed;
+            _currentSpeed =  Mathf.Lerp(_currentSpeed, _targetSpeed, _config.AcceleratingSpeed * Time.fixedDeltaTime);
+            
+            var movementForce = transform.forward * (_movementInput.y * Mathf.Abs(_currentSpeed));
             _rb.AddForce(movementForce);
         }
 
         private void ApplySteeringTorque()
         {
-            var steeringAngle = _movementInput.x * _config.MaxWheelTurnAngle;
-            var steeringTorque = steeringAngle * _config.SteeringSpeed;
+            if (_movementInput.y == 0)
+            {
+                return;
+            }
+            
+            var rotationAmount = _movementInput.x * -_config.MaxWheelTurnAngle;
+            
+            if (_movementInput.y > 0)
+            {
+                rotationAmount *= -1;
+            }
+            
+            var steeringTorque = rotationAmount * _config.SteeringSpeed;
+
             _rb.AddTorque(Vector3.up * steeringTorque);
         }
         
@@ -83,8 +113,23 @@ namespace Logic
 
         private void ApplyWheelRotating()
         {
-            var rotationAmount = _movementInput.x * -_config.MaxWheelTurnAngle;
-            _steeringWheel.localRotation = Quaternion.Euler(0f, 0f, rotationAmount);
+            var speedRatio = Mathf.Abs(_rb.velocity.magnitude) / _config.MaxSpeed;
+            var xRotationAmount = _movementInput.x * -_config.MaxWheelTurnAngle;
+            var yRotationAmount = speedRatio * _config.MaxWheelRotationSpeed;
+            yRotationAmount *= Vector3.Dot(_rb.velocity, transform.forward) > 0 ? 1 : -1;
+
+            foreach (var wheel in _frontWheels)
+            {
+                wheel.Rotate(yRotationAmount, 0f, 0f);
+            }
+            
+            foreach (var wheel in _backWheels)
+            {
+                wheel.Rotate(yRotationAmount, 0f, 0f);
+                wheel.localRotation = quaternion.Euler(0f, xRotationAmount, 0f);
+            }
+            
+            _steeringWheel.localRotation = Quaternion.Euler(0f, 0f, xRotationAmount);
         }
         
         private void RaiseForks()
--- Assets/_Scripts/ForkliftConfig.cs
+++ Assets/_Scripts/ForkliftConfig.cs
@@ -1,22 +1,26 @@
 using UnityEngine;
 
-namespace Data
+namespace Forklift
 {
     [CreateAssetMenu(fileName = "Forklift Config", menuName = "Configs/Forklift Config")]
     public class ForkliftConfig : ScriptableObject
     {
         [SerializeField] private float _acceleratingSpeed;
-        [SerializeField] private float _maxWheelTurnAngle;
-        [SerializeField] private float _centerMassOffset;
+        [SerializeField] private float _maxSpeed;
         [SerializeField] private float _steeringSpeed;
+        [SerializeField] private float _maxWheelTurnAngle;
+        [SerializeField] private float _maxWheelRotationSpeed;
+        [SerializeField] private Vector3 _centerMassOffset;
         [SerializeField] private float _frictionCoefficient;
         [SerializeField] private float _forkLiftingSpeed;
         [SerializeField] private Vector2 _liftMinMaxPositions;
 
         public float AcceleratingSpeed => _acceleratingSpeed;
-        public float MaxWheelTurnAngle => _maxWheelTurnAngle;
-        public float CenterMassOffset => _centerMassOffset;
+        public float MaxSpeed => _maxSpeed;
         public float SteeringSpeed => _steeringSpeed;
+        public float MaxWheelTurnAngle => _maxWheelTurnAngle;
+        public float MaxWheelRotationSpeed => _maxWheelRotationSpeed;
+        public Vector3 CenterMassOffset => _centerMassOffset;
         public float FrictionCoefficient => _frictionCoefficient;
         public float ForkLiftingSpeed => _forkLiftingSpeed;
         public Vector2 LiftMinMaxPositions => _liftMinMaxPositions;
